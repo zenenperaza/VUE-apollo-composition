51f88fd1dd308260274bf9e9aa01e56470e785c6
zenenperaza <peraza@outlook.com>
2023-07-02 22:16:08 -0400

    commits composition API

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
-<h1>hell world</h1>
+<h1>Comments</h1>
+<div v-if="loading">
+ <h3>Loadong ....</h3>
+</div>
+<div v-else>
+<ul>
+  <li v-for="(comment, index) in result.getCommentsFromUser" :key="index"><b> {{ comment.name }}</b>: {{ comment.text }}</li>
+</ul>
+</div>
+<div v-if="error">
+<h3>{{error.message}}</h3>
+</div>
+<button v-on:click="refetch()">Refresh</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { useQuery } from "@vue/apollo-composable";
+import { useQuery, useLazyQuery } from "@vue/apollo-composable";
 import gpl from 'graphql-tag'
 
 export default defineComponent({
   setup() {
-    const {result, loading, error} = useQuery(gpl`
-    query {
-      getAllComments {
-        name
-        text
-      }
+    const {result, loading, error, refetch, onResult, onError} = useQuery(gpl`
+    query GetCommentsFromUser($name: String!) {
+    getCommentsFromUser(name: $name) {
+      text
+      name
     }
-    `)
+  }
+    `, () => ({
+      name: "User 1"
+    }), {
+      fetchPolicy: 'cache-and-network',
+      pollInterval: 5000
+    })
+
+    onResult((queryResult) => {
+      console.log(queryResult.data);
+      console.log(queryResult.loading);
+      console.log(queryResult.networkStatus);
+      
+    })
+
+    onError((error) => {
+      console.log(error.graphQLErrors);
+      
+    })
+
+
     return {
-      result, loading, error
+      result, loading, error, refetch
     }
   }
 })
